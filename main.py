17a44f866dcb566f1304ee0c2d5d079183778fc8
Carlos Pereira <carloshnpwork@gmail.com>
2024-03-11 07:10:49 -0300

    feat: transaction schema

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
+from pydantic import BaseModel
+import datetime
 
 app = FastAPI()
 client = MongoClient("mongodb://localhost:27017/")
 db = client["mydatabase"]
+
+# Schema
+class Transaction(BaseModel):
+    transaction_id: int
+    merchant_id: int
+    user_id: int
+    card_number: str
+    transaction_date: datetime.datetime
+    transaction_amount: float
+    device_id: int
+    has_cbk: bool
 
 @app.get("/")
 async def root():
