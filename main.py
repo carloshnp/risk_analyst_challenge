1a4c2b755d7c84be4eed2c5e8c6ef9c95c59a931
Carlos Pereira <carloshnpwork@gmail.com>
2024-03-11 08:30:20 -0300

    feat: db collections + chargeback rule

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
+from typing import Union
 import datetime
 
 app = FastAPI()
-client = MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
+client = AsyncIOMotorClient("mongodb://db:27017/")
+db = client['risk_analysis_db']
 
+# Access your collections
+chargebacks = db['chargebacks']
+transactions = db['transactions']
 # Schema
 class Transaction(BaseModel):
     transaction_id: int
@@ -17,8 +22,22 @@ class Transaction(BaseModel):
     transaction_amount: float
     device_id: int
     has_cbk: bool
-    
+
 async def analyze_transaction(transaction: Transaction):
+    # Checks if transaction has chargeback
+    print(transaction)
+    if transaction.has_cbk:
+        result["recommendation"] = "Deny"
+        result["reason"] = "Transaction flagged as fraudulent (has chargeback)"
+    else:
+        chargeback_history = await db.chargebacks.find_one({"user_id": transaction.user_id})
+        if chargeback_history:
+            result["recommendation"] = "Deny"
+            result["reason"] = "Previous chargeback history"
+    
+    # Record the transaction in the "transactions" collection
+    await db.transactions.insert_one(transaction.model_dump())
+    
     result = {"recommendation": "Approve"}
     return transaction, result
 
