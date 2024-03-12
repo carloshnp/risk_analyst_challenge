5ce61fa8d6ac330042ec886901114ca36747803e
Carlos Pereira <carloshnpwork@gmail.com>
2024-03-11 21:50:34 -0300

    feat: controller layer

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-import streamlit as st
+from fastapi import FastAPI
+from controllers.transaction_controller import router
 
-st.title('My first app')
+app = FastAPI() 
+
+app.include_router(router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -1,21 +1,17 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from services.transaction_service import TransactionService
 from repositories.transaction_repository import TransactionRepository
 from models.transaction_schema import Transaction
 
-app = FastAPI()
+router = APIRouter(prefix="/api/transactions", tags=["transactions"])  # Create APIRouter
 
 transaction_repository = TransactionRepository()
 transaction_service = TransactionService(transaction_repository)
 
-@app.post("/analyze_transaction/")
+@router.post("/analyze_transaction/")
 async def analyze(transaction: Transaction, service: TransactionService = Depends(lambda: transaction_service)):
-    try:
-        result = await service.analyze_transaction(transaction)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Error analyzing transaction")
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+  try:
+    result = await service.analyze_transaction(transaction) 
+    return result
+  except Exception as e:
+    raise HTTPException(status_code=500, detail="Error analyzing transaction")
